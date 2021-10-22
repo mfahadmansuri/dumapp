32638c4f1daa315034ed516de904daaa369a83ae
mfahadmansuri <73505834+mfahadmansuri@users.noreply.github.com>
2021-10-22 13:47:22 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from pywebio import STATIC_PATH
+from flask import Flask, send_from_directory
+from pywebio.input import *
+from pywebio.output import *
+from pywebio import start_server
+import pandas as pd
+import dtale
+import argparse
+
+def dapp():
+    userfile = file_upload('upload data file')
+    open(userfile['filename'],'wb').write(userfile['content'])
+    df=pd.read_csv(userfile['filename'])
+    put_text(df.head())
+    col=list(df.columns)
+    Target_column = checkbox("Column to encoded with Dummies", options=col)
+    datatf=pd.get_dummies(df,columns=Target_column)
+    d = dtale.show(datatf)
+    d.open_browser()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    args = parser.parse_args()
+
+    start_server(dapp, port=args.port)
